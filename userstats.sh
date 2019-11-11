444617cc454c45e38199c938d008438f1e2fe4c2
hugofpaiva <hugofpaiva@ua.pt>
2019-11-11 14:50:11 +0000

    Tratamento de opções updated com getopts. Falta testar quando são mandadas várias opções de uma vez

--- userstats.sh
+++ userstats.sh
@@ -14,22 +14,50 @@ function read_file(){
 }
 read_file
 
+# Usage - Como se usa o script
+function usage(){
+   echo "BURRO"
+}
+
+
+
+#Tratamento de opções
 # -z: vai testar se o "$1" é uma string nula ou não. Se for uma string nula, é executado.
 #    [ -z "$1" ] ) 
+if [ -z "$1" ]#Este if verifica se é passada algum arguemto ou não
+then 
+   echo "Nenhum argumento ou opção"
+   exit
+else
 
-# As opções são passadas todas a seguir ao getopts. Se tiver ":" quer dizer que aceita argumentos. O "${OPTARG}" são os argumentos
+   while getopts g:u:s:e:f:rntai option; do # As opções são passadas todas a seguir ao getopts. Se tiver ":" quer dizer que aceita argumentos. O "${OPTARG}" são os argumentos
+      case "${option}" in
+         g) echo "A opção g foi ativada." ;;
+         u) argumento_b="${OPTARG}" ;;
+         s) recebi_c=1 ;;
+         e) recebi_d=1 ;;
+         f) echo "O script vai ler do ficheiro ${OPTARG}";;
+         r) ;;
+         n) ;;
+         t) ;;
+         a) ;;
+         i) ;;
+         *)
+               echo "*"
+               ;;
+      esac
+      shift $((OPTIND-1))
+   done
 
-while getopts g:u:s:e:f:rntai option; do
-     case "${option}" in
-        g) echo "A opção A foi ativada." ;;
-        u) argumento_b="${OPTARG}" ;;
-        s) recebi_c=1 ;;
-        e) recebi_d=1 ;;
-        f) recebi_e=1 ;;
-        r) ;;
-        n) ;;
-        t) ;;
-        a) ;;
-        i) ;;
-     esac
-  done
+   if [ $OPTIND -eq 1 ] #Este if corre se não houve nenhuma opção mas forem passado argumentos
+   then
+      echo "Nenhuma opção mas foram passado argumentos"
+   fi
+
+  fi
+
+
+# Este shitf vai fazer desaparecer dos argumentos $1, $2, ... as opções e argumentos passado ao getopts
+
+#Logo, ao fazer echo "$1" vai-me dar os outros argumentos não utilizados em getopts
+#echo "$1"
