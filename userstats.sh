32523c863f059f2b8ccd2295f30e40a1db2cbc9e
carolinaaraujo00 <carolina.araujo00@uua.pt>
2019-11-19 16:19:45 +0000

    updates

--- userstats.sh
+++ userstats.sh
@@ -5,15 +5,9 @@ path="last.txt"
 
 # Arrays para guardar users e a sua informaçao
 users=()
+file_array=()
+declare -A argsinfo=()
 declare -A userinfo=()
-
-# Leitura de ficheiro:
-function read_file() {
-   while IFS= read -r line; do
-      file_array+=("$line")
-   done <"$path"
-
-}
 
 # Usage das opções - Como se usa o script
 function usage() {
@@ -29,6 +23,102 @@ function usage() {
    exit
 }
 
+# Tratamento de opções
+function args(){
+   if [ -z "$1" ]; then 
+      getUsers
+      getInfo
+
+      for u in "${userinfo[@]}"; do
+         printf "%s %s\n" "$u" "${userinfo[$u]}"
+      done
+      
+
+   exit
+
+   else
+
+      while getopts g:u:s:e:f:rntai option; do
+         case "${option}" in
+         g)
+            if [ ${2:0:1} == "-" ]; then 
+               usage
+            else
+               echo "A opção g foi ativada."
+               $(last)
+            fi
+            ;;
+         u)
+            echo ${2:0:1}
+            if [ ${2:0:1} == "-" ]; then 
+               usage
+            else
+               echo "ok"
+               argumento_b="${OPTARG}"
+            fi
+            ;;
+         s)
+            if [ ${2:0:1} == "-" ]; then 
+               usage
+            else
+               recebi_c=1
+               echo "s"
+            fi
+            ;;
+         e)
+                    $(last)
+            if [ ${2:0:1} == "-" ]; then 
+               usage
+            else
+               echo "e"
+               recebi_d=1
+            fi
+            ;;
+         f)
+            if [ ${2:0:1} == "-" ]; then 
+               usage
+            else
+               echo "O script vai ler do ficheiro ${OPTARG}"
+
+               exit
+            fi
+            ;;
+         r)
+            echo "ok"
+            ;;
+         n) ;;
+         t) ;;
+         a) ;;
+         i) ;;
+         *)
+            usage
+            ;;
+         esac
+      done
+
+      if [ $OPTIND -eq 1 ]; then
+         echo " "
+         echo "Nenhuma opção mas foram passado argumentos"
+         echo " "
+         usage
+      fi
+
+      shift $((OPTIND - 1))
+
+
+   fi
+
+}
+
+# Leitura de ficheiro:
+function read_file() {
+   while IFS= read -r line; do
+      file_array+=("$line")
+   done <"$path"
+
+}
+
+
 # Tratamento de dados
 function getUsers(){
    users=$(last -f cenas | awk '{print $1}' | sort -u | head -n -1 | sort | sed '/reboot/d' | grep -v '^$'z1)   
@@ -36,8 +126,6 @@ function getUsers(){
 }
 
 function calcTime(){
-   # 0x:xx / 00:xx / 00:0x / xx:xx          - length 5
-   # 1+0x:xx / 1+00:xx / 1+00:0x / 1+xx:xx  - length 7
    time=$1
 
    # Calcular tempo em minutos
@@ -77,88 +165,3 @@ function getInfo(){
 
    done
 }
-
-# Tratamento de opções
-if [ -z "$1" ]; then 
-   getUsers
-   getInfo
-
-   for u in "${userinfo[@]}"; do
-      printf "%s %s\n" "$u" "${userinfo[$u]}"
-   done
-   
-
-exit
-
-else
-
-   while getopts g:u:s:e:f:rntai option; do
-      case "${option}" in
-      g)
-         if [ ${2:0:1} == "-" ]; then 
-            usage
-         else
-            echo "A opção g foi ativada."
-            $(last)
-
-         fi
-         ;;
-      u)
-         echo ${2:0:1}
-         if [ ${2:0:1} == "-" ]; then 
-            usage
-         else
-
-            echo "ok"
-            argumento_b="${OPTARG}"
-         fi
-         ;;
-      s)
-         if [ ${2:0:1} == "-" ]; then 
-            usage
-         else
-            recebi_c=1
-            echo "s"
-         fi
-         ;;
-      e)
-         if [ ${2:0:1} == "-" ]; then 
-            usage
-         else
-            echo "e"
-            recebi_d=1
-         fi
-         ;;
-      f)
-         if [ ${2:0:1} == "-" ]; then 
-            usage
-         else
-            echo "O script vai ler do ficheiro ${OPTARG}"
-
-            exit
-         fi
-         ;;
-      r)
-         echo "ok"
-         ;;
-      n) ;;
-      t) ;;
-      a) ;;
-      i) ;;
-      *)
-         usage
-         ;;
-      esac
-   done
-
-   if [ $OPTIND -eq 1 ]; then
-      echo " "
-      echo "Nenhuma opção mas foram passado argumentos"
-      echo " "
-      usage
-   fi
-
-   shift $((OPTIND - 1))
-
-
-fi
