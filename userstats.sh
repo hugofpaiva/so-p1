24e4af9245f7dc0276c344f764b7e69b6a332963
carolinaaraujo00 <carolina.araujo00@uua.pt>
2019-11-19 15:12:02 +0000

    tempos, users, funcoes

--- userstats.sh
+++ userstats.sh
@@ -1,40 +1,21 @@
 #!/bin/bash
 
-#path="/var/log/wtmp"
+# path="/var/log/wtmp"
 path="last.txt"
 
-#Arrays para a leitura de ficheiro:
-
-file_array=() #Array onde vai ser guardado o conteúdo do ficheiro
-users=()      #Array onde vão ser guardados os utilizadores
-users_unique=()
-group=()            #Array onde vão ser guardados os grupos de utilizadores
-session=()          #Array onde vai ser guardado o tempo total de ligação
-init_data=()        #Array onde vai ser guardada a data de início da sessão
-final_hour=()       #Array onde vai ser guardada a hora de fim da sessão
-num_users_unique=() #Array onde vai ser guardado o número de users únicos e os users
-init_hour=()        #Array onde vai ser guardado a hora de início de sessão
-min_session=()      #Array onde vai ser guardado o tempo total de ligação (minutos)
+# Arrays para guardar users e a sua informaçao
+users=()
+declare -A userinfo=()
 
 # Leitura de ficheiro:
-# -r: opção passada para o comando read que evita o "backslash escape" de ser interpretado
-# IFS=:Opção antes do comando read que previne os espaços de serem cortados
 function read_file() {
    while IFS= read -r line; do
       file_array+=("$line")
-      #IFS=' ' read -r -a array <<< "$line" #outra maneira de ler
    done <"$path"
-   #Estou a colocar os elementos do file_array no array. Faço o split com espaço
-   #IFS=' ' read -r -a array <<<"$file_array" #outra maniera de ler
-   #echo "${array[0]}"
-   #readarray a < $path
-   #echo "${a[60]}"
 
 }
 
-#read_file
-
-# Usage - Como se usa o script
+# Usage das opções - Como se usa o script
 function usage() {
    echo "Usage: $0 -g [grupo] -u [nome] -s [data1] -p [data2] -r -n -t -a -i"
    echo ""
@@ -49,52 +30,72 @@ function usage() {
 }
 
 # Tratamento de dados
+function getUsers(){
+   users=$(last -f cenas | awk '{print $1}' | sort -u | head -n -1 | sort | sed '/reboot/d' | grep -v '^$'z1)   
+   users=$(echo $users | tr " " "\n")
+}
 
-init_data+=$(last | awk '!/^wtmp/{print $3 "\t" $4 "\t" $5 "\t" $6}')
-init_hour+=$(last | awk '!/^wtmp/{print $6}')
-final_hour+=$(last | awk '!/^wtmp/{print $8}')
-session+=$(last | awk '!/^wtmp/{print substr($9,2,5)}') #### O !/^wtmp/ ignora a linha que começa por wtmp e por consequência, a linha em branco anterior
-min_session+=$(echo "${session[@]}" | awk '{print (((substr($1,0,2)*60)+substr($1,4,5)))}')
-users+="$(last | awk '!/^wtmp/{print $1}')"                 # o | manda o comando last para o awk e é guarda a info no array users
-users_unique+=$(echo "${users[@]}" | tr ' ' '\n' | sort -u) #array apenas com unique users #uniq -c #space (‘ ‘) is replaced by tab (‘\t’), fazemos isto pq o sort compara linhas #-u:only output the first of
-#a sequence of lines that compare equal
-num_users_unique+=$(
-   IFS=$'\n'
-   sort <<<"${users[*]}" | uniq -c
-)
-#echo $(echo "${session[*]}" | awk '{print $2 " " $1}')
-echo ${min_session[@]}
-#echo "${session[@]}"
-#echo "${users[@]}"
-###################### TESTES ##############################
-#Ir buscar o count do user do array tem_users que tem a contagem e o nome dos user únicos
-#for each in "${temp_users[@]}"
-#do
-# num_users+=$(echo "$each" | awk '{print $2}')
-#done
-#ou
-#num_users+=$(echo "${temp_users[*]}" | awk '{print $1}')
-#print do num_users
-#echo "${#num_users[*]}"# vai buscar o length do array por causa do "#"
-#echo "${num_users[*]}"
-############################################################
+function calcTime(){
+   # 0x:xx / 00:xx / 00:0x / xx:xx          - length 5
+   # 1+0x:xx / 1+00:xx / 1+00:0x / 1+xx:xx  - length 7
+   time=$1
 
-#Tratamento de opções
-# -z: vai testar se o "$1" é uma string nula ou não. Se for uma string nula, é executado.
-#    [ -z "$1" ] )
-if [ -z "$1" ]; then #Este if verifica se é passada algum arguemto ou não. Tem de ter espaços a toda a volta do "[" "]"
-   #echo "Nenhum argumento ou opção"
-   #Print das opções de acordo com o pedido na opção:
-   printf "%s %s \n" "$(echo "${num_users_unique[*]}" | awk '{print $2 " " $1}')" #Vai trocar a order do num_users_unique para fazer display corretamente
+   # Calcular tempo em minutos
+   if (( ${#time} == 7)); then
+      minlogged=$(echo $time | tr '+' ':' | awk -F: '{ print ($1 * 1440) + ($2 * 60) + $3 }')
+   else
+      minlogged=$(echo $time | awk -F: '{ print ($1 * 60) + $2 }')
+   fi
 
-   exit
+   # Calcular o total
+   total=$(($total + $minlogged))
+
+   # Calcular máximo e mínimo
+   if (( minlogged < min )); then
+      min=$minlogged
+   fi
+
+   if (( minlogged > max )); then
+      max=$minlogged
+   fi
+}
+
+function getInfo(){
+   for user in $users; do
+      sessions=$(last -f cenas | grep -o $user | wc -l)
+      time=$(last -f cenas | grep $user | awk '{print $10}' | sed '/in/d' | sed 's/[)(]//g')
+
+      min=3000000
+      max=0
+      total=0
+
+      for t in $time; do
+         calcTime "$t"
+      done
+
+      userinfo[$user]="${user} ${sessions} ${total} ${max} ${min}"
+
+   done
+}
+
+# Tratamento de opções
+if [ -z "$1" ]; then 
+   getUsers
+   getInfo
+
+   for u in "${userinfo[@]}"; do
+      printf "%s %s\n" "$u" "${userinfo[$u]}"
+   done
+   
+
+exit
 
 else
 
-   while getopts g:u:s:e:f:rntai option; do # As opções são passadas todas a seguir ao getopts. Se tiver ":" quer dizer que aceita argumentos. O "${OPTARG}" são os argumentos
+   while getopts g:u:s:e:f:rntai option; do
       case "${option}" in
       g)
-         if [ ${2:0:1} == "-" ]; then #Verifica se o 1º caracter do 2º argumento é um "-", ou seja, uma opção. Se isto não acontecer, quer dizer que é um argumento e a função corre normal.
+         if [ ${2:0:1} == "-" ]; then 
             usage
          else
             echo "A opção g foi ativada."
@@ -104,7 +105,7 @@ else
          ;;
       u)
          echo ${2:0:1}
-         if [ ${2:0:1} == "-" ]; then #Verifica se o 1º caracter do 2º argumento é um "-", ou seja, uma opção. Se isto não acontecer, quer dizer que é um argumento e a função corre normal.
+         if [ ${2:0:1} == "-" ]; then 
             usage
          else
 
@@ -113,7 +114,7 @@ else
          fi
          ;;
       s)
-         if [ ${2:0:1} == "-" ]; then #Verifica se o 1º caracter do 2º argumento é um "-", ou seja, uma opção. Se isto não acontecer, quer dizer que é um argumento e a função corre normal.
+         if [ ${2:0:1} == "-" ]; then 
             usage
          else
             recebi_c=1
@@ -121,7 +122,7 @@ else
          fi
          ;;
       e)
-         if [ ${2:0:1} == "-" ]; then #Verifica se o 1º caracter do 2º argumento é um "-", ou seja, uma opção. Se isto não acontecer, quer dizer que é um argumento e a função corre normal.
+         if [ ${2:0:1} == "-" ]; then 
             usage
          else
             echo "e"
@@ -129,24 +130,16 @@ else
          fi
          ;;
       f)
-         if [ ${2:0:1} == "-" ]; then #Verifica se o 1º caracter do 2º argumento é um "-", ou seja, uma opção. Se isto não acontecer, quer dizer que é um argumento e a função corre normal.
+         if [ ${2:0:1} == "-" ]; then 
             usage
          else
             echo "O script vai ler do ficheiro ${OPTARG}"
-            init_data+=$(last -f ${OPTARG} | awk '{print $3 "\t" $4 "\t" $5 "\t" $6}')
-            users+=($(last -f ${OPTARG} | awk '{print $1}'))              # o | manda o comando last para o awk e é guarda a info no array users
-            users_unique+=($(echo "${users[@]}" | tr ' ' '\n' | sort -u)) #array apenas com unique users #uniq -c #space (‘ ‘) is replaced by tab (‘\t’), fazemos isto pq o sort compara linhas #-u:only output the first of
-            #a sequence of lines that compare equal
-            # print da info para testes
-            for i in "${init_data[@]}"; do
-               :
-               echo "$i"
-            done
+
             exit
          fi
          ;;
       r)
-         echo "oko"
+         echo "ok"
          ;;
       n) ;;
       t) ;;
@@ -158,7 +151,7 @@ else
       esac
    done
 
-   if [ $OPTIND -eq 1 ]; then #Este if corre se não houve nenhuma opção mas forem passado argumentos
+   if [ $OPTIND -eq 1 ]; then
       echo " "
       echo "Nenhuma opção mas foram passado argumentos"
       echo " "
@@ -167,8 +160,5 @@ else
 
    shift $((OPTIND - 1))
 
-fi
 
-#shift $((OPTIND-1)) Este shitf vai fazer desaparecer dos argumentos $1, $2, ... as opções e argumentos passado ao getopts
-#Logo, ao fazer echo "$1" vai-me dar os outros argumentos não utilizados em getopts
-#echo "$1"
+fi
