2c6d6ae3df07da6f2d36dfb511a55cb488721059
hugofpaiva <hugofpaiva@ua.pt>
2019-11-22 21:41:48 +0000

    Várias funcionalidades implementadas

--- userstats.sh
+++ userstats.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# path="/var/log/wtmp"
-path="last.txt"
-
 # Arrays para guardar users e a sua informaçao
 users=()
 file_array=()
-declare -A argsinfo=()
-declare -A userinfo=()
+declare -A argOpt=()
+declare -A userInfo=()
 
 # Usage das opções - Como se usa o script
 function usage() {
    echo "Usage: $0 -g [grupo] -u [nome] -s [data1] -p [data2] -r -n -t -a -i"
    echo ""
    echo "[grupo] = Grupo de utilizadores"
-   echo "[nome] = Nome dos utilizadores"
+   echo "[nome]  = Nome dos utilizadores"
    echo "[data1] = Data de início da sessão a partir da qual as sessões devem ser consideradas"
    echo "[data2] = Data de início de sessão a partir da qual as sessões não devem ser consideradas"
    echo ""
@@ -25,53 +22,34 @@ function usage() {
 
 # Tratamento de opções
 function args(){
-   if [ -z "$1" ]; then 
-      getUsers
-      getInfo
-
-      for u in "${userinfo[@]}"; do
-         printf "%s %s\n" "$u" "${userinfo[$u]}"
-      done
-      
-
-   exit
-
-   else
-
-      while getopts g:u:s:e:f:rntai option; do
-         case "${option}" in
+   while getopts g:u:s:e:f:rntai option; do
+      case "${option}" in
          g)
             if [ ${2:0:1} == "-" ]; then 
                usage
             else
-               echo "A opção g foi ativada."
-               $(last)
+               echo "Selecting users that belong to the ${OPTARG} group"
             fi
             ;;
          u)
-            echo ${2:0:1}
             if [ ${2:0:1} == "-" ]; then 
                usage
             else
-               echo "ok"
-               argumento_b="${OPTARG}"
+               echo "Selecting users that match ${OPTARG}"
             fi
             ;;
          s)
             if [ ${2:0:1} == "-" ]; then 
                usage
             else
-               recebi_c=1
-               echo "s"
+               echo "Opção s"
             fi
             ;;
-         e)
-                    $(last)
+         e)   
             if [ ${2:0:1} == "-" ]; then 
                usage
             else
-               echo "e"
-               recebi_d=1
+               echo "Opção e"
             fi
             ;;
          f)
@@ -79,62 +57,76 @@ function args(){
                usage
             else
                echo "O script vai ler do ficheiro ${OPTARG}"
-
-               exit
             fi
             ;;
-         r)
-            echo "ok"
-            ;;
-         n) ;;
-         t) ;;
-         a) ;;
-         i) ;;
+         r|n|t|a|i) ;;
          *)
             usage
             ;;
          esac
-      done
 
-      if [ $OPTIND -eq 1 ]; then
-         echo " "
-         echo "Nenhuma opção mas foram passado argumentos"
-         echo " "
-         usage
+      if [[ -z "$OPTARG" ]]; then
+         argOpt[$option]="none"
+      else
+         argOpt[$option]=${OPTARG}
       fi
 
-      shift $((OPTIND - 1))
-
-
-   fi
+   done
+   shift $((OPTIND - 1))
 
 }
-
-# Leitura de ficheiro:
-function read_file() {
-   while IFS= read -r line; do
-      file_array+=("$line")
-   done <"$path"
-
-}
-
 
 # Tratamento de dados
 function getUsers(){
-   users=$(last -f cenas | awk '{print $1}' | sort -u | head -n -1 | sort | sed '/reboot/d' | grep -v '^$'z1)   
-   users=$(echo $users | tr " " "\n")
+   if [[ -v argOpt[f] ]]; then
+      users=$(last -f "${argOpt['f']}" | awk '{print $1}' | sort | uniq | sed '/reboot/d' | sed "/${argOpt['f']}/d" )   
+   else
+         # Filtrar users
+      if [[ -v argOpt[u] ]]; then
+         match="${argOpt['u']}"
+         users=$(last | awk '{print $1}' | sort | uniq | sed '/reboot/d' | sed '/wtmp/d' | grep $match)
+      
+      elif [[ -v argOpt[g] ]]; then
+         group="${argOpt['g']}"
+         users=$(last | awk '{print $1}' | sort | uniq | sed '/reboot/d' | sed '/wtmp/d')
+         
+         index=0
+         for u in ${users[@]}; do
+            userGroups=($( id -G -n $u ))
+            if ! [[ ${userGroups[@]} =~ $group ]]; then 
+               unset users[$index]
+               index=$((index+1))
+            fi
+         done
+
+      elif [[ -v argOpt[s] || -v argOpt[e] ]]; then
+         start=$(date -d "${argOpt['s']}" +"%Y-%m-%d ")
+         start+="$(echo "${argOpt['s']}" | awk '{print $3}')"
+         start=" -s \"$start\" "
+
+         end=$(date -d "${argOpt['e']}" +"%Y-%m-%d ")
+         end+="$(echo "${argOpt['e']}" | awk '{print $3}')"
+         end=" -t \"$end\" "
+
+         users=$(eval last $start $end | awk '{print $1}' | sort | uniq | sed '/reboot/d' | sed '/wtmp/d')
+      else
+         users=$(last | awk '{print $1}' | sort | uniq | sed '/reboot/d' | sed '/wtmp/d')
+      fi
+   fi
+
+
+
 }
 
-function calcTime(){
+function calculateTime(){
    time=$1
 
    # Calcular tempo em minutos
-   if (( ${#time} == 7)); then
+   if (( ${#time} >= 7)); then
       minlogged=$(echo $time | tr '+' ':' | awk -F: '{ print ($1 * 1440) + ($2 * 60) + $3 }')
    else
       minlogged=$(echo $time | awk -F: '{ print ($1 * 60) + $2 }')
    fi
-
    # Calcular o total
    total=$(($total + $minlogged))
 
@@ -148,20 +140,61 @@ function calcTime(){
    fi
 }
 
-function getInfo(){
-   for user in $users; do
-      sessions=$(last -f cenas | grep -o $user | wc -l)
-      time=$(last -f cenas | grep $user | awk '{print $10}' | sed '/in/d' | sed 's/[)(]//g')
+function getUserInfo(){ 
+   echo "I may take a while to process, but I'll get there. Please have a little faith!"
+
+   for user in ${users[@]}; do
+      if [[ -v argOpt[f] ]]; then
+         sessions=$(last -f "${argOpt['f']}" | grep -o $user | wc -l)
+         time=$(last -f "${argOpt['f']}" | grep $user | awk '{print $10}' | sed '/in/d' | sed 's/[)(]//g') 
+      else
+         sessions=$(last | grep -o $user | wc -l)
+         time=$(last | grep $user | awk '{print $10}' | sed '/in/d' | sed 's/[)(]//g')
+      fi
 
       min=3000000
       max=0
-      total=0
+      total=0   
 
       for t in $time; do
-         calcTime "$t"
+         calculateTime "$t"
       done
 
-      userinfo[$user]="${user} ${sessions} ${total} ${max} ${min}"
-
+      userInfo[$user]=$(printf "%-8s %-5s %-6s %-5s %-5s\n" "$user" "$sessions" "$total" "$max" "$min")
    done
+   printIt
 }
+
+function printIt(){
+   if [[ -v argOpt[r] ]]; then 
+      # ordem decrescente(nome user)
+      order="-r"
+   else
+      order=""
+   fi
+
+   if [[ -v argOpt[n] ]]; then
+      # ordenar por numero de sessoes
+      printf "%s\n" "${userInfo[@]}" | sort -k1,1n ${order}
+   
+   elif [[ -v argOpt[t] ]]; then
+      # por tempo total
+      printf "%s\n" "${userInfo[@]}" | sort -k2,2n ${order}
+   
+   elif [[ -v argOpt[a] ]]; then
+      # por tempo máximo
+      printf "%s\n" "${userInfo[@]}" | sort -k3,3n ${order}
+   
+   elif [[ -v argOpt[i] ]]; then
+      # por tempo mínimo
+      printf "%s\n" "${userInfo[@]}" | sort -k5,5n ${order}
+   
+   else 
+      #ordem crescente (nome user)
+      printf "%s\n" "${userInfo[@]}" | sort -k1,1n ${order}
+   fi
+}
+
+args "$@"
+getUsers
+getUserInfo
